7830532745bc81feab819157686ce4d7020bc036
Augusto Lorencatto <augusto.lorencatto@ecomenergia.com.br>
2025-01-28 06:11:48 -0300

    Adicionando receita

---
+++ calc_nutricional/calc_nutricional/doctype/receita/receita.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Augusto Lorencatto and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Receita", {
+// 	refresh(frm) {
+
+// 	},
+// });
